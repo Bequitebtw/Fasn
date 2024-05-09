2fe079b8ca1316102e125f8db277f08eb0f4553e
Bequitebtw <Bequitebtw@mail.ru>
2024-05-09 23:08:50 +0300

    main page frame

--- lib/design/images.dart
+++ lib/design/images.dart
@@ -1,3 +1,8 @@
 import 'package:flutter_svg/flutter_svg.dart';
 
 final SvgPicture googleIcon = SvgPicture.asset('/images/googleIcon.svg');
+final SvgPicture logo = SvgPicture.asset('/images/logo.svg');
+final SvgPicture heart = SvgPicture.asset('/images/heart.svg');
+final SvgPicture profile = SvgPicture.asset('/images/user.svg');
+final SvgPicture cart = SvgPicture.asset('/images/cart.svg');
+final SvgPicture home = SvgPicture.asset('/images/home.svg');
--- lib/pages/app/test.scree.dart
+++ lib/pages/app/test.scree.dart
@@ -1,49 +1,164 @@
+import 'package:fasn/design/colors.dart';
+import 'package:fasn/design/images.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../main.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  int currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
     final user = supabase.auth.currentUser;
     final profileImageUrl = user?.userMetadata?['avatar_url'];
     final fullName = user?.userMetadata?['full_name'];
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            if (profileImageUrl != null)
-              ClipOval(
-                child: Image.network(
-                  profileImageUrl,
-                  width: 100,
-                  height: 100,
-                  fit: BoxFit.cover,
-                ),
+        backgroundColor: primaryColor,
+        appBar: AppBar(
+            backgroundColor: primaryColor,
+            automaticallyImplyLeading: false,
+            title: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "F",
+                        style: TextStyle(
+                            fontFamily: "Popins",
+                            fontSize: 24,
+                            fontWeight: FontWeight.w400,
+                            color: secondaryColor),
+                      ),
+                      logo,
+                      Text(
+                        "sn",
+                        style: TextStyle(
+                            fontFamily: "Popins",
+                            fontSize: 24,
+                            fontWeight: FontWeight.w400,
+                            color: secondaryColor),
+                      ),
+                    ],
+                  ),
+                  Icon(
+                    Icons.notifications_outlined,
+                    color: secondaryColor,
+                  )
+                ],
               ),
-            const SizedBox(height: 16),
-            Text(
-              fullName ?? '',
-              style: Theme.of(context).textTheme.headlineMedium,
+            )),
+        body: <Widget>[
+          /// Home page
+          ListView(
+            children: [],
+          ),
+          ListView(
+            children: [],
+          ),
+
+          /// Notifications page
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                Card(
+                  child: ListTile(
+                    leading: Icon(Icons.notifications_sharp),
+                    title: Text('Notification 1'),
+                    subtitle: Text('This is a notification'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: Icon(Icons.notifications_sharp),
+                    title: Text('Notification 2'),
+                    subtitle: Text('This is a notification'),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 32),
-            TextButton(
-              onPressed: () async {
-                await supabase.auth.signOut();
-                if (context.mounted) {
-                  Navigator.pushReplacementNamed(
-                    (context),
-                    '/',
-                  );
-                }
-              },
-              child: const Text('Sign out'),
-            )
+          ),
+
+          /// Messages page
+          ListView.builder(
+            reverse: true,
+            itemCount: 2,
+            itemBuilder: (BuildContext context, int index) {
+              if (index == 0) {
+                return Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    margin: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Text(
+                      'Hello',
+                    ),
+                  ),
+                );
+              }
+              return Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  margin: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Text(
+                    'Hi!',
+                  ),
+                ),
+              );
+            },
+          ),
+        ][currentPageIndex],
+        bottomNavigationBar: NavigationBar(
+          overlayColor: MaterialStatePropertyAll(primaryColor),
+          backgroundColor: primaryColor,
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          selectedIndex: currentPageIndex,
+          destinations: const <Widget>[
+            NavigationDestination(
+              icon: Icon(Icons.home_outlined, color: secondaryColor),
+              label: 'home',
+            ),
+            NavigationDestination(
+              icon: Badge(
+                label: Text('2'),
+                child: Icon(Icons.messenger_sharp, color: secondaryColor),
+              ),
+              label: 'cart',
+            ),
+            NavigationDestination(
+              icon: Badge(
+                child: Icon(Icons.abc, color: secondaryColor),
+              ),
+              label: "asd",
+            ),
+            NavigationDestination(
+              icon: Icon(
+                Icons.account_box,
+                color: secondaryColor,
+              ),
+              label: "account",
+            ),
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
